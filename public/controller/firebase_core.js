26a665557595e4aaa0b1d78bd0df11a7c01b6654
ywong8 <ywong8@uco.edu>
2024-08-22 17:56:12 -0500

    initial commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBdMooF2iu_jJK9bQuATLLftz1B2NpcF3c",
+    authDomain: "ywong8-cmsc4373-webapp.firebaseapp.com",
+    projectId: "ywong8-cmsc4373-webapp",
+    storageBucket: "ywong8-cmsc4373-webapp.appspot.com",
+    messagingSenderId: "692649550269",
+    appId: "1:692649550269:web:56686033022d947cd6f2d4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
